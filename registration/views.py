a19fed7660a0dc589cc62bc4f39e65e137f4cad8
Ezequiel <ezequielmazariegossolis@gmail.com>
2024-09-28 16:58:09 -0600

    Se añade la vista de registro en views.py

--- registration/views.py
+++ registration/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from .forms import RegistroForm
 
-# Create your views here.
+def registro(request):
+    if request.method == 'POST':
+        form = RegistroForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)  # Iniciar sesión automáticamente tras el registro
+            return redirect('nombre_de_la_vista_principal')  # Cambia 'nombre_de_la_vista_principal' por la vista a la que quieras redirigir
+    else:
+        form = RegistroForm()
+
+    return render(request, 'registration/registro.html', {'form': form})
