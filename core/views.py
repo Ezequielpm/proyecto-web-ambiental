034f625ed24f29356a21f20950387340d94b3ddc
Ezequiel <ezequielmazariegossolis@gmail.com>
2024-10-02 21:54:21 -0600

    Se crea la vista basada en clases para la vista de subir articulo

--- core/views.py
+++ core/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render,HttpResponse
 from django.views.generic import ListView
 from .models import Articulo
+from django.views.generic.edit import CreateView
+from .forms import ArticuloForm
+from django.urls import reverse_lazy
 # Create your views here.
 def inicio(request):
     return render(request,'core/index.html');
@@ -29,3 +32,13 @@ class ArticuloViewAll(ListView):
     paginate_by = 4    
     ordering = ['-fecha_subida']
 
+
+class ArticuloCreateView(CreateView):
+    model = Articulo
+    form_class = ArticuloForm
+    template_name = 'core/articulo_form.html'
+    success_url = reverse_lazy('articulos-view-all')  # Redirigir a una página de éxito
+
+    def form_valid(self, form):
+        form.instance.usuario = self.request.user  # Asignar el usuario actual al artículo
+        return super().form_valid(form)
