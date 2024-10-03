6042f1035eeedfec80d44a1df52035a74be4b263
Ezequiel <ezequielmazariegossolis@gmail.com>
2024-10-02 21:53:50 -0600

    Se añade el path de la vista subir articulo

--- core/urls.py
+++ core/urls.py
@@ -6,10 +6,12 @@
 
 from django.urls import path
 from .views import ArticuloListView,ArticuloDetailView,ArticuloViewAll
+from .views import ArticuloCreateView
 
 urlpatterns = [
     path('', ArticuloListView.as_view(), name='articulo-list'),
     path('articulo/<int:pk>/', ArticuloDetailView.as_view(), name='articulo-detail'),
     path('articulosdisponibles',ArticuloViewAll.as_view(),name="articulos-view-all"),
+    path('subir-articulo/', ArticuloCreateView.as_view(), name='upload-articulo'),
 ]
 
