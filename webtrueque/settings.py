b3f2c65d81f65b8664065ed5d8793ffbd36bcea5
Ezequiel <ezequielmazariegossolis@gmail.com>
2024-10-05 22:22:18 -0600

    se agrega USE_L10N = True al settings.py

--- webtrueque/settings.py
+++ webtrueque/settings.py
@@ -112,7 +112,7 @@ TIME_ZONE = 'America/Mexico_City'
 USE_I18N = True
 
 USE_TZ = True
-
+USE_L10N = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
