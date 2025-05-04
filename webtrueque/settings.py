cf9f1061695cb5b9d4fe767c989f5b353387ef53
Ezequielpm <ezequielmazariegossolis@gmail.com>
2025-05-03 20:02:15 -0600

    Update settings.py

--- webtrueque/settings.py
+++ webtrueque/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-nn4nj=ous&umugx!&ueaw)f*d(#974vsg!d!*a@aljkfn@gi@x'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     'ezequieltrueque.pythonanywhere.com',
